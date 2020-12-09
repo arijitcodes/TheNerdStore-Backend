dfd3c9a5168c25b1a90c9ea62191c8e2875a91e4
Arijit Banerjee <72866688+arijitcodes@users.noreply.github.com>
2020-12-09 23:18:22 +0530

    Initial Commit: Setup of basic code structe - mainly backend, and User Schema

---
+++ models/user.js
@@ -0,0 +1,78 @@
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+const { v5: uuid } = require("uuid");
+
+// Defining User Schema
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    maxlength: 32,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    maxlength: 32,
+    trim: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+  },
+  userInfo: {
+    type: String,
+    trim: true,
+  },
+  encryptedPassword: {
+    type: String,
+    required: true,
+  },
+  salt: String,
+  role: {
+    type: Number,
+    default: 0,
+  },
+  purchases: {
+    type: Array,
+    default: [],
+  },
+});
+
+// Defining Virtual Functions - setters and getters
+userSchema
+  .virtual()
+  .set(function (password) {
+    this._password = password;
+    this.salt = uuid();
+    this.encryptedPassword = this.securePassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+
+// Defining Schema Methods
+userSchema.method = {
+  //
+  // To Authenticate, returns true if encrypted password and input password are same
+  authenticate: function (plainPassword) {
+    return this.securePassword(plainPassword) === this.encryptedPassword;
+  },
+
+  // Takes a Plain Text password as input and returns Encrypted Password
+  securePassword: function (plainPassword) {
+    if (!plainPassword) return "";
+
+    try {
+      return crypto
+        .createHmac("sha256", this.salt)
+        .update(plainPassword)
+        .digest("hex");
+    } catch (error) {
+      return "";
+    }
+  },
+};
+
+module.exports = mongoose.model("Users", userSchema);
