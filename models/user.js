9b3c6f4ad1a6e1069f14553a18c52fd33ed0f194
Arijit Banerjee <72866688+arijitcodes@users.noreply.github.com>
2020-12-09 23:54:38 +0530

    Mod: Modified User Schema

--- models/user.js
+++ models/user.js
@@ -3,42 +3,45 @@ const crypto = require("crypto");
 const { v5: uuid } = require("uuid");
 
 // Defining User Schema
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    maxlength: 32,
-    trim: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      maxlength: 32,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      maxlength: 32,
+      trim: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    userInfo: {
+      type: String,
+      trim: true,
+    },
+    encryptedPassword: {
+      type: String,
+      required: true,
+    },
+    salt: String,
+    role: {
+      type: Number,
+      default: 0,
+    },
+    purchases: {
+      type: Array,
+      default: [],
+    },
   },
-  lastName: {
-    type: String,
-    maxlength: 32,
-    trim: true,
-  },
-  email: {
-    type: String,
-    trim: true,
-    required: true,
-    unique: true,
-  },
-  userInfo: {
-    type: String,
-    trim: true,
-  },
-  encryptedPassword: {
-    type: String,
-    required: true,
-  },
-  salt: String,
-  role: {
-    type: Number,
-    default: 0,
-  },
-  purchases: {
-    type: Array,
-    default: [],
-  },
-});
+  { timestamps: true }
+);
 
 // Defining Virtual Functions - setters and getters
 userSchema
