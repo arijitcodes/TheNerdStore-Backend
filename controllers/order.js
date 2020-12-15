3c3dd9121cb4076e68f4137aea18d5979de5f0d6
Arijit Banerjee <72866688+arijitcodes@users.noreply.github.com>
2020-12-16 02:21:00 +0530

    Add & Mod: Added 'createOrder' Route and Controller in Order

--- controllers/order.js
+++ controllers/order.js
@@ -2,7 +2,7 @@ const { Order, ProductCart } = require("../models/order");
 
 // Middlewares
 
-// Parameter Extractors
+// Parameter Extractor Middleware
 exports.getOrderById = (req, res, next, id) => {
   Order.findById(id)
     .populate("products.product", "name price")
@@ -14,4 +14,18 @@ exports.getOrderById = (req, res, next, id) => {
       req.order = order;
       next();
     });
+};
+
+// Controller Methods
+exports.createOrder = (req, res) => {
+  req.body.order.user = req.profile;
+
+  const order = new Order(req.body.order);
+  order.save((error, order) => {
+    if (error) {
+      return res.status(400).json({ err: "Failed to save order in DB!" });
+    }
+
+    res.json(order);
+  });
 };
--- routes/order.js
+++ routes/order.js
@@ -5,11 +5,25 @@ const router = express.Router();
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 const { getUserById, pushOrderInPurchaseList } = require("../controllers/user");
 const { updateStock } = require("../controllers/product");
-const { getOrderById } = require("../controllers/order");
+const { getOrderById, createOrder } = require("../controllers/order");
 
 // Parameter Extractor Middlewares
 
 router.param("userId", getUserById);
 router.param("orderId", getOrderById);
+
+// Routes
+
+// @Route:  POST - /order/create/:userId
+// @Desc:   Create a new Order
+// @Access: Private
+router.post(
+  "/order/create/:userId",
+  isSignedIn,
+  isAuthenticated,
+  pushOrderInPurchaseList,
+  updateStock,
+  createOrder
+);
 
 module.exports = router;
