0b21d0b28083a94555c745ce98103dddba354a20
Arijit Banerjee <72866688+arijitcodes@users.noreply.github.com>
2020-12-15 18:20:19 +0530

    Add & Mod: Added 'createProduct' Route & Controller. Added Product Photo upload - handling and field's upload restrictions.

--- controllers/product.js
+++ controllers/product.js
@@ -1,4 +1,7 @@
 const Product = require("../models/product");
+const formidable = require("formidable");
+const _ = require("lodash");
+const fs = require("fs");
 
 // Middlewares
 exports.getProductById = (req, res, next, id) => {
@@ -12,4 +15,50 @@ exports.getProductById = (req, res, next, id) => {
       req.product = product;
       next();
     });
+};
+
+// Controller Methods
+
+// Create Product
+exports.createProduct = (req, res) => {
+  let form = formidable.IncomingForm();
+  form.keepExtensions = true;
+
+  form.parse(req, (error, fields, file) => {
+    if (error) {
+      return res
+        .status(400)
+        .json({ err: "Problem with Image. Upload failed!" });
+    }
+
+    // De-structure the incoming fields
+    const { name, description, price, category, stock } = fields;
+
+    // Restrictions on field
+    if (!name || !description || !price || !category || !stock) {
+      return res.status(400).json({ err: "Please include all the fields!" });
+    }
+
+    let product = new Product(fields);
+
+    // Handling Files
+    if (file.photo) {
+      // Checking file size
+      if (file.photo.size > 3000000) {
+        return res.status(400).json({ err: "File size is too big!" });
+      }
+
+      product.photo.data = fs.readFileSync(file.photo.path);
+      product.photo.contentType = file.photo.type;
+    }
+
+    // Save in DB
+    product.save((error, product) => {
+      if (error) {
+        return res.status(400).json({ err: "Could not save Product in DB!" });
+      }
+
+      res.json(product);
+    });
+  });
 };
--- routes/product.js
+++ routes/product.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 // Controller Components
-const { getProductById } = require("../controllers/product");
+const { getProductById, createProduct } = require("../controllers/product");
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 const { getUserById } = require("../controllers/user");
 
@@ -11,5 +11,16 @@ router.param("userId", getUserById);
 router.param("productId", getProductById);
 
 // Routes
+
+// @Route:  POST - /product/create/userId
+// @Desc:   Create a new Product
+// @Access: Private (Admin Only)
+router.post(
+  "/product/create/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  createProduct
+);
 
 module.exports = router;
